e0bc9a93a8143b6a21ade56d3ea1cdd608bbc13a
DevVedant <vedantgurao53@gmail.com>
2021-08-26 17:52:10 +0530

    d1p2 done

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,48 @@
+import { style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+@Component({
+  selector: 'app-test',
+  templateUrl:'./test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  public name1 ="vedant";
+  // public siteurl = window.location.href;
+  // public myid="test id";
+  // public isdisable=false;
+  // public sucsessclass="text-success";
+  // public haserror=false;
+    //  public xyz = true;
+    //  public greeting: string;
+    //  public multistyle= {
+    //    color:"red",
+    //    fontstyle:"italic"
+    public name="";
+
+    constructor() {
+      console.log('Hi from constrcture');
+
+    }
+
+    ngOnInit(): void {
+
+    }
+
+     }
+
+
+
+
+//   onClick(event){
+//     console.log(event);
+//     this.greeting =event.type;
+//   }
+//  greetuser(){
+//   return "hello "  + this.name1;
+// }
+// logMessage(value){
+//   console.log(value);
+// }
+// }
